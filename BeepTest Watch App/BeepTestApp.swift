dfb6150c510d895276bf7306d6d2ca398364c8f8
James O'Donoghue <jameso@astra.com>
2023-11-12 12:59:02 -0800

    chore: first commit

---
+++ BeepTest Watch App/BeepTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeepTestApp.swift
+//  BeepTest Watch App
+//
+//  Created by James O'Donoghue on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BeepTest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeepTest/BeepTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeepTestApp.swift
+//  BeepTest
+//
+//  Created by James O'Donoghue on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BeepTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
